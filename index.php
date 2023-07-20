e718f4ffe33aeb3328311f9ef9823556cc386354
Monika Rathore <monirathore125@email.com>
2023-07-20 20:58:27 +0530

    Index file

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Form Clone</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>
+                Google From Clone
+            </h1>
+        </div>
+        <!-- form -->
+        <div class="form">
+            <form action="" class="formdetail">
+                <label for="firstName">FirstName :</label>
+                <input type="text" name="firstName"><br><br />
+                <label for="lastname">LastName :</label>
+                <input type="text" name="lastname"><br><br />
+                <label for="email">Email:</label>
+                <input type="text" name="email"><br><br />
+                <label for="mobile">Mobile :</label>
+                <input type="text" name="mobile"><br><br />
+                <label for="address">Address :</label>
+                <input type="text" name="address"><br><br />
+                <label for="gender">Gender :</label>
+                <input type="radio" name="gender" value="male">
+                <label for="male">Male</label>
+                <input type="radio" name="gender" value="female">
+                <label for="female">Female</label>
+                <input type="radio" name="gender" value="other">
+                <label for="other">Other</label><br /><br />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
